a5bfb1aaacc79d4f876e6a28da63b2595333bb7c
Ubuntu <ubuntu@ip-172-31-13-108.eu-west-1.compute.internal>
2021-03-10 19:01:23 +0000

    Ansible!!!!!

--- ejemplos/aws1/main.tf
+++ ejemplos/aws1/main.tf
@@ -17,8 +17,17 @@ provider "aws" {
 }
 
 resource "tls_private_key" "clave_privada" {
-  algorithm   = "RSA"
-  rsa_bits    = "4096"
+    algorithm   = "RSA"
+    rsa_bits    = "4096"
+    
+    provisioner "local-exec" {
+        command = "echo \"${self.private_key_pem}\" > clave_privada.pem"
+    }
+
+    provisioner "local-exec" {
+        command = "echo \"${self.public_key_pem}\" > clave_publica.pem"
+    }
+
 }
 
 resource "aws_key_pair" "claves_aws" {
@@ -54,8 +63,24 @@ resource "aws_security_group" "reglas_red_ivan" {
 }
 
 
+data "aws_ami" "ami_ubuntu" {
+    most_recent = true
+    owners = [ "099720109477" ]
+    
+    filter {
+        name   ="name"
+        values = [ "*ubuntu-xenial-16.04-amd64-server-*" ]
+    }
+    
+    filter {
+        name   = "virtualization-type"
+        values = ["hvm"]
+    }
+}
+
+
 resource "aws_instance" "mi-maquina-ivan" {
-    ami           = "ami-05573edad5dd1a926"
+    ami           = data.aws_ami.ami_ubuntu.id
     instance_type = "t2.micro"
     key_name      = aws_key_pair.claves_aws.key_name
     
@@ -66,23 +91,41 @@ resource "aws_instance" "mi-maquina-ivan" {
     security_groups = [
         aws_security_group.reglas_red_ivan.name
     ]
-    
-    connection {
-        type         = "ssh"
-        host         = self.public_ip
-        user         = "ubuntu"
-        private_key  = tls_private_key.clave_privada.private_key_pem
-        port         = 22
+
+    provisioner "local-exec" {
+        command =  "echo \"${self.public_ip} ansible_connection=ssh ansible_port=22 ansible_user=ubuntu ansible_ssh_private_key_file=./clave_privada.pem\" > inventario.ini"
     }
     
-    provisioner "remote-exec" {
-        inline = [ "docker run -p 8080:8080 -d bitnami/tomcat" ]
+    provisioner "local-exec" {
+        command =  "ansible-playbook -i inventario.ini mi-playbook.yaml"
     }
+
+
+
+
+#    user_data = <<-EOF
+#        #!/bin/bash
+#        sudo apt-get update -y
+#        EOF
+    
+#    connection {
+#        type         = "ssh"
+#        host         = self.public_ip
+#        user         = "ubuntu"
+#        private_key  = tls_private_key.clave_privada.private_key_pem
+#        port         = 22
+#    }
+    
+#    provisioner "remote-exec" {
+#        inline = [ "sudo apt-get update", "sudo apt-get install docker.io -y ", "docker run -p 8080:8080 -d bitnami/tomcat" ]
+#    }
 }
+
 
 output "mi-clave-privada" {
     value = tls_private_key.clave_privada.private_key_pem
 }
+
 output "mi-clave-publica" {
     value = tls_private_key.clave_privada.public_key_pem
 }
